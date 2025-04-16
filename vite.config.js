7617ca9012115d10961da4730cfeb541e443c289
IvanMouses <mavsis@mail.ru>
2025-04-16 17:32:32 +0300

    vite.config.js change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/friendly-dental-studio",
+  base: "/",
   plugins: [react()],
 });
